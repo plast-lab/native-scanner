e1bca27e11291e958b2d678a91598e4762b46a9c
George Fourtounis <gfour@di.uoa.gr>
2020-02-18 16:55:09 +0200

    Add test.

---
+++ src/test/java/org/clyze/scanner/BasicTest.java
@@ -0,0 +1,57 @@
+package org.clyze.scanner;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+
+    private static String getTestNativeLibrary() {
+        final String LIB_RESOURCE = "/libjackpal-androidterm5.so";
+        try {
+            Path tmpPath = Files.createTempFile("lib", ".so");
+            InputStream resourceAsStream = BasicTest.class.getResourceAsStream(LIB_RESOURCE);
+            Files.copy(resourceAsStream, tmpPath, StandardCopyOption.REPLACE_EXISTING);
+            return tmpPath.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Error: could not extract " + LIB_RESOURCE);
+        }
+    }
+
+    @Test
+    public void libraryStringsMatch() {
+        CheckPair p = new CheckPair();
+        NativeDatabaseConsumer dbc = new NativeDatabaseConsumer() {
+                    @Override
+                    public void add(String predicateFile, String arg, String... args) {
+                        // System.out.println(predicateFile + "('" + args[1] + "')");
+                        if (predicateFile.equals(BinaryAnalysis.NATIVE_METHODTYPE_CANDIDATE) &&
+                            args[1].equals("(IIIII)V")) {
+                            System.out.println("Found: " + args[1]);
+                            p.methodTypeFound = true;
+                        } else if (predicateFile.equals(BinaryAnalysis.NATIVE_NAME_CANDIDATE) &&
+                                   args[1].equals("setPtyWindowSizeInternal")) {
+                            System.out.println("Found: " + args[1]);
+                            p.methodNameFound = true;
+                        }
+                    }
+            };
+        // NativeDatabaseConsumer dbc = new BasicDatabaseConsumer();
+
+        String libPath = getTestNativeLibrary();
+        boolean radareMode = false;
+        boolean onlyPreciseStrings = false;
+        boolean truncateAddresses = false;
+        boolean demangle = false;
+        BinaryAnalysis analysis = NativeScanner.create(dbc, radareMode, libPath, onlyPreciseStrings, truncateAddresses, demangle);
+        (new NativeScanner(null)).scanBinaryCode(analysis);
+
+        assertTrue(p.methodNameFound);
+        assertTrue(p.methodTypeFound);
+    }
+}
---
+++ src/test/java/org/clyze/scanner/CheckPair.java
@@ -0,0 +1,6 @@
+package org.clyze.scanner;
+
+class CheckPair {
+    public boolean methodNameFound = false;
+    public boolean methodTypeFound = false;
+}
